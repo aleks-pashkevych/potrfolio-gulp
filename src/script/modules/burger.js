92e007cbddd61b14bbd31ea797f3e843480f0420
Aleksandr Pashkevych <alekspashkevych@gmail.com>
2023-02-24 23:13:19 +0200

    feat: burger-menu complete

--- src/script/modules/burger.js
+++ src/script/modules/burger.js
@@ -6,12 +6,17 @@ const burgerClose = document.querySelector(".burger__close");
 
 let burgerStatus = "close";
 
+const burgerMenu = document.querySelector(".header__menu");
+
 const burgerAction = () => {
   const openBurgerMenu = () => {
     burgerIcon.classList.remove("active");
     burgerIcon.classList.add("hide");
     burgerClose.classList.remove("hide");
     burgerClose.classList.add("active");
+    document.body.classList.add("lock");
+    burgerMenu.classList.remove("burger__closing");
+    burgerMenu.classList.add("burger__open");
     burgerStatus = "open";
   };
   const closeBurgerMenu = () => {
@@ -19,6 +24,9 @@ const burgerAction = () => {
     burgerIcon.classList.add("active");
     burgerClose.classList.remove("active");
     burgerClose.classList.add("hide");
+    document.body.classList.remove("lock");
+    burgerMenu.classList.remove("burger__open");
+    burgerMenu.classList.add("burger__closing");
     burgerStatus = "close";
   };
 
