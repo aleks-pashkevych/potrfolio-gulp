03d661d6fda4555b3e8aa8db7338fe86b87af7c8
Aleks Pashkevych <alekspashkevych@gmail.com>
2023-03-01 16:18:45 +0200

    feat: desc msg +, remove post not working

--- src/script/modules/feedback.js
+++ src/script/modules/feedback.js
@@ -1,8 +1,9 @@
 const submitBtn = document.querySelector(".form__submit");
-
+const binImg = document.querySelectorAll(".bin");
+const binBtn = document.querySelectorAll(".messages__icon");
 const messageData = {};
 let messages = [];
-
+let color = "black";
 const getDataFromForm = () => {
   const userNameInput = document.querySelector("#FName");
   const emailInput = document.querySelector("#email");
@@ -18,42 +19,83 @@ const getDataFromForm = () => {
   messageData.time = `${currentTime.getHours()} : ${currentTime.getMinutes()}`;
 };
 
-const setMessage = () => {
-  const messageSection = document.querySelector(".messages__wrapper");
-  const singleMessage = `<h2 class="messages__headline">Messages: , time: ${messageData.time}</h2>`;
-  messages.push(singleMessage);
-  let data = "";
-  messages.forEach((message) => {
-    data += message;
-  });
-  messageSection.innerHTML = data;
-  console.log(messages);
+const removeDiv = () => {
+  console.log("removeDiv");
+  for (let i = 0; i < binBtn.length; i++) {
+    // console.log(binBtn.length);
+    binBtn[i].onclick = () => {
+      console.log(`BTN: ${i}`);
+      messages.splice(i, 1);
+      putFormData();
+      console.log(messages);
+    };
+  }
 };
+
+removeDiv();
+// binBtn.onclick = (e) => {
+//   for (let i = 0; i < binBtn.length; i++) {
+//     if (binBtn[i] == e) {
+//       console.log(binBtn[i]);
+//     }
+//   }
+// };
 const formAction = () => {
+  removeDiv();
   submitBtn.onclick = (e) => {
     e.preventDefault();
     getDataFromForm();
     putFormData();
-    setMessage();
+    removeDiv();
+
     return false;
   };
 };
 
+// const binMoveAction = () => {
+//   binBtn.forEach((btn) => {
+//     btn.addEventListener("mouseover", (e) => {
+//       color = "red";
+//       putFormData();
+//     });
+//     btn.addEventListener("mouseout", (e) => {
+//       color = "black";
+//       putFormData();
+//     });
+//   });
+// };
+
 const putFormData = () => {
-  console.log(messageData);
-  const dataSection = document.createElement("div");
-
-  const messagesSection = document.querySelector(".messages__wrapper");
-  dataSection.classList.add("contacts__description");
-
-  const timeElement = document.createElement("p");
-  timeElement.classList.add("fedback_time");
-
-  const textElement = document.createTextNode(messageData.user);
-  dataSection.appendChild(textElement);
-  messagesSection.appendChild(dataSection);
-  // console.log(textElement.value);
-  // messagesSection.appendChild(textElement);
+  const messagesSection = document.querySelector(".messages__container");
+  const theMessage = document.createElement("div");
+  theMessage.innerHTML = `
+  <div class="messages__wrapper">
+  <div class="messages__text">
+  <p class="messages__headtext">${messageData.user}: </p>
+  <p class="messages__content">
+    ${messageData.message}
+  </p>
+  <p class="messages__time">${messageData.time}, ${messageData.date}</p>
+</div>
+<div class="messages__icon">
+  <img
+    src="assets/images/bin-${color}.svg"
+    alt="Del"
+    width="40px"
+    height="40px"
+    class="bin"
+  />
+</div>
+</div>
+`;
+  // console.log(theMessage);
+  messages.push(theMessage);
+  // console.log(messages);
+  messages.forEach((mes) => {
+    messagesSection.innerHTML += mes.innerHTML;
+  });
+  removeDiv();
+  // console.log(messageData);
 };
 
 export default formAction;
